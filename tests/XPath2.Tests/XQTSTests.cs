61254f2bac46e8c0303b988808669e00b21b3d66
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-08-17 21:46:55 +0200

    FakeLocalTimeZone

--- tests/XPath2.Tests/XQTSTests.cs
+++ tests/XPath2.Tests/XQTSTests.cs
@@ -1,19 +1,27 @@
+using System;
 using System.Xml;
 using FluentAssertions;
 using Wmhelp.XPath2;
+using XPath2.Tests.Utils;
 using Xunit;
 
 namespace XPath2.Tests
 {
     public class XQTSTests
     {
+        /// <summary>
+        /// Test that the implicit timezone in the dynamic context is used if $timezone is empty; indirectly also tests context stability.
+        /// </summary>
         [Fact]
         public void XPath2Evaluate_AdjDateTimeToTimezoneFunc_6()
         {
-            var nav = new XmlDocument().CreateNavigator();
-            var result2 = nav.XPath2Evaluate("timezone-from-dateTime(adjust-dateTime-to-timezone(xs:dateTime(\"2001-02-03T00:00:00\"))) eq implicit-timezone()");
+            using (new FakeLocalTimeZone(TimeZoneInfo.Utc))
+            {
+                var nav = new XmlDocument().CreateNavigator();
+                var result = nav.XPath2Evaluate("timezone-from-dateTime(adjust-dateTime-to-timezone(xs:dateTime(\"2001-02-03T00:00:00\"))) eq implicit-timezone()");
 
-            result2.Should().Be(true);
+                result.Should().Be(true);
+            }
         }
     }
 }
