d3a409276ef52948dfbd573cb52fe0bacb4e2eb5
Stef Heyenrath <stef.heyenrath@gmail.com>
2023-11-28 20:41:26 +0100

    nuget + net8

--- src/XQTSRunConsole/Program.cs
+++ src/XQTSRunConsole/Program.cs
@@ -2,35 +2,34 @@ using System;
 using System.IO;
 using XPath2.TestRunner;
 
-namespace XQTSRunConsole
+namespace XQTSRunConsole;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var passedWriter = args.Length > 1 ? TextWriter.Synchronized(new StreamWriter(args[1])) : null; // Needs to be Synchronized
+        var errorWriter = args.Length > 2 ? TextWriter.Synchronized(new StreamWriter(args[2])) : null; // Needs to be Synchronized
+        var runner = new XQTSRunner(Console.Out, passedWriter, errorWriter);
+
+        //var result1 = runner.Run(args[0], RunType.Parallel);
+        //Console.WriteLine("{0} / {1} = {2}%", result1.Passed, result1.Total, result1.Percentage);
+
+        var result2 = runner.Run(args[0], RunType.Sequential);
+
+        // 15133 executed, 12958 (85.63%) succeeded.
+        Console.WriteLine("{0} / {1} = {2}%", result2.Passed, result2.Total, result2.Percentage);
+
+        if (passedWriter != null)
         {
-            var passedWriter = args.Length > 1 ? TextWriter.Synchronized(new StreamWriter(args[1])) : null; // Needs to be Synchronized
-            var errorWriter = args.Length > 2 ? TextWriter.Synchronized(new StreamWriter(args[2])) : null; // Needs to be Synchronized
-            var runner = new XQTSRunner(Console.Out, passedWriter, errorWriter);
+            passedWriter.Flush();
+            passedWriter.Close();
+        }
 
-            //var result1 = runner.Run(args[0], RunType.Parallel);
-            //Console.WriteLine("{0} / {1} = {2}%", result1.Passed, result1.Total, result1.Percentage);
-
-            var result2 = runner.Run(args[0], RunType.Sequential);
-
-            // 15133 executed, 12958 (85.63%) succeeded.
-            Console.WriteLine("{0} / {1} = {2}%", result2.Passed, result2.Total, result2.Percentage);
-
-            if (passedWriter != null)
-            {
-                passedWriter.Flush();
-                passedWriter.Close();
-            }
-
-            if (errorWriter != null)
-            {
-                errorWriter.Flush();
-                errorWriter.Close();
-            }
+        if (errorWriter != null)
+        {
+            errorWriter.Flush();
+            errorWriter.Close();
         }
     }
 }
